aec7a158958884586342f7011d5150aae6760d2f
leo li <leo.li@nagrastar.com>
2018-11-01 16:35:28 -0600

    create search_bar and render in index.js, setup Youtube API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = ''; // need to put in key
+
 // create a new component. this component should produce some HTML
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 // take the component and put it on the page
