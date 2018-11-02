9f353ca69b67c0a6f009a81673e7119242d54523
leo li <leo.li@nagrastar.com>
2018-11-02 11:45:30 -0600

    learn about eventHandler

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react'; // {Component} that way when we extends the class below we can use Component instead of React.Component
 
-const SearchBar = () => {
-    return <input />;
-};
+// { Component }  === const Component = React.Component
+
+// functional component
+// const SearchBar = () => {
+//     return <input />;
+// };
+
+// class based component can keep track of some sort of records, state
+class SearchBar extends Component {
+    render() {
+        // the event handler passed back into onChange, onChange is prop
+        return <input onChange={this.onInputChange} />;
+    }
+
+    // Event Handler , pass in event object, we can refactor to put the eventHandler function directly into the onChange
+    onInputChange(event) {
+        console.log(event.target.value);
+    }
+}
 
 export default SearchBar;
