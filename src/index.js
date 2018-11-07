758374c7565c06b186c70ab2d213651e54ebc1fb
leolee1986 <leolee1986@gmail.com>
2018-11-06 21:18:27 -0700

    install lodash and set debounce

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
@@ -29,9 +30,12 @@ class App extends Component {
     }
 
     render() {
+        // use lodash to slow down the term, only kick on every 300 ms or so allow user to type
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+
         return (
             <div>
-                <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+                <SearchBar onSearchTermChange={videoSearch}/>
                 <VideoDetail video = {this.state.selectedVideo}/>
                 <VideoList
                  onVideoSelect={selectedVideo => this.setState({selectedVideo})}
